5575066e21f163a73669312265eec5a21e8c532a
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-07-13 15:34:41 +0300

    работает первый тест

--- tests/api/rest/todo/test_get_todos.py
+++ tests/api/rest/todo/test_get_todos.py
@@ -1,4 +1,5 @@
 import pytest
+
 from config.config_general import config_general
 
 # from api.requests.todo_request import TodoRequest
@@ -8,6 +9,9 @@ from testrunner.base_test import BaseTest
 @pytest.mark.usefixtures('delete_all_todos')
 class TestGetTodos(BaseTest):
     def test_get_todos_with_existing_entries(self):
-        print(config_general.base_url)
+        """Получение пустого списка TODO при пустой БД"""
         response = self.http_session.get(url=self.base_url + '/todos')
-        print(response.json())
+
+        assert response.status_code == 200
+        body = response.json()
+        assert len(body) == 0
