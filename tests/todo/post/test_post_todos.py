6b6c30729646c29be78593824be7a69bca0de5ae
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-07-14 17:03:00 +0300

    убрано лишнее

--- tests/todo/post/test_post_todos.py
+++ tests/todo/post/test_post_todos.py
@@ -11,7 +11,7 @@ from testrunner.base_test import BaseTest
 
 @pytest.mark.usefixtures('delete_all_todos_scope_test')
 class TestPostTodos(BaseTest):
-    def test_create_todo_with_valid_data(self):
+    def test_update_existing_todo_(self):
         new_todo = ToDo(id=1, text='New Task', completed=False)
 
         response = self.http_session.post(
@@ -33,65 +33,3 @@ class TestPostTodos(BaseTest):
                 found = True
                 break
         assert found, 'Созданная задача не найдена в списке TODO'
-
-    def test_create_todo_with_missing_fialds(self):
-        new_todo = {'id': 2, 'completed': True}
-
-        response = self.http_session.post(
-            url=self.base_url + Endpoints.todos,
-            data=json.dumps(new_todo),
-            headers={'Content-Type': 'application/json'},
-        )
-        assert response.status_code == HTTPStatus.BAD_REQUEST
-        assert response.headers.get('Content-Type') == 'text/plain; charset=utf-8'
-        assert response.text
-
-    def test_create_todo_with_max_length_text(self):
-        max_length_text = ''.join('A' for i in range(255))
-        new_todo = ToDo(id=3, text=max_length_text, completed=False)
-
-        response = self.http_session.post(
-            url=self.base_url + Endpoints.todos,
-            data=json.dumps(asdict(new_todo)),
-            headers={'Content-Type': 'application/json'},
-        )
-        assert response.status_code == HTTPStatus.CREATED
-        assert response.text == ''
-
-        response = self.http_session.get(url=self.base_url + Endpoints.todos)
-        assert response.status_code == HTTPStatus.OK
-        body = response.json()
-
-        found = False
-        for todo in body:
-            if todo.get('id') == new_todo.id:
-                assert todo.get('text') == new_todo.text
-                assert todo.get('completed') is False
-                found = True
-                break
-        assert found, 'Созданная задача не найдена в списке TODO'
-
-    def test_create_todo_with_invalid_data_types(self):
-        new_todo = {'id': 4, 'text': 'Invalid Data Typ', 'completed': 'notBoolean'}
-
-        response = self.http_session.post(
-            url=self.base_url + Endpoints.todos,
-            data=json.dumps(new_todo),
-            headers={'Content-Type': 'application/json'},
-        )
-        assert response.status_code == HTTPStatus.BAD_REQUEST
-        assert response.text
-
-    def test_create_todo_with_existing_id(self):
-        first_todo = ToDo(id=5, text='First Task', completed=False)
-        self.create_todo(todo_data=first_todo)
-
-        duplicated_todo = ToDo(id=5, text='Duplicated Task', completed=True)
-
-        response = self.http_session.post(
-            url=self.base_url + Endpoints.todos,
-            data=json.dumps(asdict(duplicated_todo)),
-            headers={'Content-Type': 'application/json'},
-        )
-        assert response.status_code == HTTPStatus.BAD_REQUEST
-        assert response.text
