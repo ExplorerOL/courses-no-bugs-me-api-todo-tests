24a6cfcf402e1526a466e953dff3322d6b25c631
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-07-16 17:39:40 +0300

    добавлены комментарии секций подготовки, шагов и проверок

--- tests/todo/get/test_get_todos.py
+++ tests/todo/get/test_get_todos.py
@@ -12,7 +12,9 @@ from tests.todo.base_test import BaseTest
 class TestGetTodos(BaseTest):
     def test_get_todo_when_database_is_empty(self, validated_todo_request_anonim: ValidatedToDoRequest):
         """Получение пустого списка TODO, когда база данных пуста"""
+        # ACT
         todos = validated_todo_request_anonim.read_all()
+        # ASSERT
         assert len(todos) == 0
 
     def test_get_todos_with_existing_entries(
@@ -21,8 +23,9 @@ class TestGetTodos(BaseTest):
         created_ten_or_more_todos_with_random_data: list[ToDo],
     ):
         """Получение списка TODO с существующими записями"""
+        # ACT
         todos = validated_todo_request_anonim.read_all()
-
+        # ASSERT
         for expected_todo, actual_todo in zip_longest(created_ten_or_more_todos_with_random_data, todos):
             assert expected_todo.id == actual_todo.id
             assert expected_todo.text == actual_todo.text
@@ -34,11 +37,12 @@ class TestGetTodos(BaseTest):
         created_ten_or_more_todos_with_random_data: list[ToDo],
     ):
         """Использование параметров offset и limit для пагинации"""
+        # ARRANGE
         limit = 2
         offset = 2
-
+        # ACT
         actual_todos = validated_todo_request_anonim.read_all(limit=limit, offset=offset)
-
+        # ASSERT
         assert len(actual_todos) == limit
         for i in range(limit):
             assert actual_todos[i].id == created_ten_or_more_todos_with_random_data[i + offset].id
@@ -46,7 +50,9 @@ class TestGetTodos(BaseTest):
 
     def test_get_todos_with_invalid_offset_and_limit(self, todo_request_anonim: ToDoRequest):
         """Передача некорректных значений в offset и limit"""
+        # ACT
         response = todo_request_anonim.read_all(limit=2, offset=-1)
+        # ASSERT
         assert response.status_code == 400
         assert 'text/plain' in response.headers['Content-Type']
         assert response.text == 'Invalid query string'
@@ -57,7 +63,7 @@ class TestGetTodos(BaseTest):
         created_ten_or_more_todos_with_random_data: list[ToDo],
     ):
         """Проверка ответа при превышении максимально допустимого значения limit"""
-
+        # ACT
         todos = validated_todo_request_anonim.read_all(limit=1000, offset=0)
-
+        # ASSERT
         assert len(todos) == len(created_ten_or_more_todos_with_random_data)
