55380c73e73ebfc51fad3b4813d97a7d88facc4a
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-07-23 15:29:56 +0300

    фикстуры переделаны с использование класса ToDoRequester

--- fixtures/fixtures_todo.py
+++ fixtures/fixtures_todo.py
@@ -2,56 +2,56 @@ import random
 
 import pytest
 
-from api.validated_todo_request import ValidatedToDoRequest
+from api.todo_requester import ToDoRequester
 from models.todo import ToDo
 from storages.tst_data_storage import TstDataStorage
 from support.generator_entities import GeneratorEntities
 
 
 @pytest.fixture(scope='function')
-def delete_all_todos_scope_test(validated_todo_request_admin: ValidatedToDoRequest) -> None:
-    todos = validated_todo_request_admin.read_all()
+def delete_all_todos_scope_test(todo_requester: ToDoRequester) -> None:
+    todos = todo_requester.validated_todo_request_admin.read_all()
     for todo in todos:
-        validated_todo_request_admin.delete(id=todo.id)
+        todo_requester.validated_todo_request_admin.delete(id=todo.id)
 
 
 @pytest.fixture(scope='session', autouse=True)
-def delete_all_todos_after_testrun_scope_session(validated_todo_request_admin: ValidatedToDoRequest):
+def delete_all_todos_after_testrun_scope_session(todo_requester: ToDoRequester):
     yield
     for id in TstDataStorage().storage.keys():
         try:
-            validated_todo_request_admin.delete(id=id)
+            todo_requester.validated_todo_request_admin.delete(id=id)
         except AssertionError:
             pass
 
 
 @pytest.fixture(scope='function')
-def created_todo_with_random_data(validated_todo_request_admin: ValidatedToDoRequest) -> ToDo:
+def created_todo_with_random_data(todo_requester: ToDoRequester) -> ToDo:
     todo = GeneratorEntities.generate_entity_with_random_data(type=ToDo)
-    validated_todo_request_admin.create(data=todo)
+    todo_requester.validated_todo_request_admin.create(data=todo)
     return todo
 
 
 @pytest.fixture(scope='function')
 def created_ten_or_more_todos_with_random_data(
     delete_all_todos_scope_test,
-    validated_todo_request_admin: ValidatedToDoRequest,
+    todo_requester: ToDoRequester,
 ) -> list[ToDo]:
     todos = [
         GeneratorEntities.generate_entity_with_random_data(type=ToDo) for _ in range(random.randint(10, 20))
     ]
     for todo in todos:
-        validated_todo_request_admin.create(data=todo)
+        todo_requester.validated_todo_request_admin.create(data=todo)
     return todos
 
 
 @pytest.fixture(scope='function')
 def created_todos_with_random_data(
     delete_all_todos_scope_test,
-    validated_todo_request_admin: ValidatedToDoRequest,
+    todo_requester: ToDoRequester,
     request,
 ) -> list[ToDo]:
     quantity = getattr(request, 'param', 10)
     todos = [GeneratorEntities.generate_entity_with_random_data(type=ToDo) for _ in range(quantity)]
-    [validated_todo_request_admin.create(data=todo) for todo in todos]
+    [todo_requester.validated_todo_request_admin.create(data=todo) for todo in todos]
     return todos
