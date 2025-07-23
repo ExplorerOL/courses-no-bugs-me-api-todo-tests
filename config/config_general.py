4447a7b6f2ab73e76790dc2993f536b87e05f58f
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-07-23 15:30:27 +0300

    добавлен таймаут в конфигурацию тестов

--- config/config_general.py
+++ config/config_general.py
@@ -6,6 +6,7 @@ class ConfigGeneral:
 
     __instance: Optional['ConfigGeneral'] = None
     __base_url: str = 'http://192.168.0.4:8080'
+    __timeout_ms: int = 10000
 
     @staticmethod
     def __new__(cls):
@@ -22,6 +23,10 @@ class ConfigGeneral:
     @base_url.setter
     def base_url(self, new_base_url: str) -> None:
         self.__base_url = new_base_url
+
+    @property
+    def timeout_ms(self) -> int:
+        return self.__timeout_ms
 
 
 config_general = ConfigGeneral()
