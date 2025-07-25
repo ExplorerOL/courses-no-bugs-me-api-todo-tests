d6b552d1c2571dd3a4bafc511138bef75bd598b9
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-07-25 13:02:56 +0300

    генератор: доработаны типы

--- support/generators/generators_entity.py
+++ support/generators/generators_entity.py
@@ -1,17 +1,15 @@
 import random
 from dataclasses import fields
-from typing import Any, TypeVar
+from typing import Any, Type
 
-from models.todo import ToDo
-from support.entities_factory import EntitiesFactory
 from support.generators.generators_string import GeneratorsString
 
-T = TypeVar('T')
+# T = TypeVar('T')
 
 
 class GeneratorsEntity:
     @staticmethod
-    def generate_entity_with_random_data(type: Any) -> Any:
+    def generate_entity_with_random_data(type: Type) -> Any:
         data = {}
         for field in fields(type):
             if field.type is bool:
@@ -23,14 +21,9 @@ class GeneratorsEntity:
             elif field.type is str:
                 data[field.name] = GeneratorsString.generate_random_string()
             else:
-                raise ValueError(f'Unsupported type: {field.type}')
+                raise ValueError(f'Неподдерживаемый тип для генерации данных: {field.type!r}!')
         return type(**data)
 
-        id = random.randint(0, 1000000000)
-        text = GeneratorsString.generate_random_string()
-        completed = random.choice([True, False])
-        return EntitiesFactory.create_entity(type=type, id=id, text=text, completed=completed)
-
     @staticmethod
-    def generate_entities_with_random_data(todo_count: int) -> list[ToDo]:
-        return [GeneratorsEntity.generate_entity_with_random_data(type=ToDo) for _ in range(todo_count)]
+    def generate_entities_with_random_data(type: Type, todo_count: int) -> list[Any]:
+        return [GeneratorsEntity.generate_entity_with_random_data(type=Type) for _ in range(todo_count)]
