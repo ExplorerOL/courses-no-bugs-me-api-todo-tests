640c7ce82be430d5d4945d8f277365b3c2e038b0
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-07-25 09:51:34 +0300

    созданы кастомные ассерты

---
+++ support/assertions/custom_assertions.py
@@ -0,0 +1,14 @@
+from contextlib import nullcontext
+from typing import Any
+
+from support.assertions import assert_soft
+
+
+class CustomAssertions:
+    @staticmethod
+    def verify_is_equal(actual_value: Any, expected_value: Any, soft: bool = True, msg: str = '') -> None:
+        """Проверка на равенство фактического и ожидаемого значений."""
+        with assert_soft if soft else nullcontext():
+            assert actual_value == expected_value, (
+                f'Фактическое значение {actual_value!r} не равно ожидаемому {expected_value!r}!'
+            )
